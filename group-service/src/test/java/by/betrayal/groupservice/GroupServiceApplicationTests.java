34be9453fc8655b4728fcc8a2d755b9027e08d27
betrayal <vogistv@gmail.com>
2024-05-03 12:50:12 +0700

    fix configurations and configure group-service

---
+++ group-service/src/test/java/by/betrayal/groupservice/GroupServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.betrayal.groupservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
